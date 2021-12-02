0c8c2cebec48c5a1ec8aed3b5fef1b1de155e243
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-12-02 23:48:28 +0200

    refactor code refactor calculator and App to functional components to use hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import calculate from './logic/calculate';
 import Calculator from './components/Calculator';
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <Calculator />
-      </>
-    );
-  }
-}
+const App = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: 0,
+  });
+
+  const clickHandler = (e) => {
+    setState((prev) => calculate(prev, e.target.value));
+  };
+
+  const { total, next } = state;
+  const stateOfCalc = { total, next };
+  return (
+    <>
+      <Calculator handleClick={clickHandler} calcState={stateOfCalc} />
+    </>
+  );
+};
+
+export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NumsOperations from './NumsOperations';
-import calculate from '../logic/calculate';
 import '../logic/operate';
 
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: 0,
-    };
+const Calculator = (props) => {
+  const { calcState, handleClick } = props;
+  const { total, next } = calcState;
+  let value = 0;
+  if (next) {
+    value = next;
+  } else if (total) {
+    value = total;
   }
 
-  clickHandler = (e) => {
-    this.setState((prevState) => calculate(prevState, e.target.value));
-  }
+  return (
+    <>
+      <input className="calc-screen" placeholder="0" value={value} readOnly />
+      <NumsOperations handleClick={handleClick} />
+    </>
+  );
+};
 
-  render() {
-    let value = 0;
-    const { total, next } = this.state;
-    if (next) {
-      value = next;
-    } else if (total) {
-      value = total;
-    }
-
-    return (
-      <>
-        <input className="calc-screen" placeholder="0" value={value} readOnly />
-        <NumsOperations handleClick={this.clickHandler} />
-      </>
-    );
-  }
-}
+export default Calculator;
