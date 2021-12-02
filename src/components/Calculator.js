dbde31924e868b90944f5eb8084d40d824aac3a7
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-12-02 13:00:27 +0200

    add calculator functionality add calculate & operate functionaliy

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import CalcScreen from './CalcScreen';
 import NumsOperations from './NumsOperations';
+import calculate from '../logic/calculate';
+import '../logic/operate';
 
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: 0,
+    };
+  }
+
+  clickHandler = (e) => {
+    this.setState((prevState) => calculate(prevState, e.target.value));
+  }
+
   render() {
+    let value = 0;
+    const { total, next } = this.state;
+    if (next) {
+      value = next;
+    } else if (total) {
+      value = total;
+    }
+
     return (
       <>
-        <CalcScreen number="0" />
-        <NumsOperations />
+        <input className="calc-screen" placeholder="0" value={value} readOnly />
+        <NumsOperations handleClick={this.clickHandler} />
       </>
     );
   }
